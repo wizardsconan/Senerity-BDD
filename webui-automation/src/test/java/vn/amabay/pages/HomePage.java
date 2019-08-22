09459fc7044e034b9d03e2320ab5477383fab9be
wizardsconan <>
2019-08-22 16:55:19 +0700

    test2

--- webui-automation/src/test/java/vn/amabay/pages/HomePage.java
+++ webui-automation/src/test/java/vn/amabay/pages/HomePage.java
@@ -11,10 +11,10 @@ public class HomePage extends PageObject{
 	@FindBy(xpath="//a[@href='/login']")
 	WebElementFacade loginLink;
 	public void clickOnLoginLink() {
-		withTimeoutOf(Duration.ofSeconds(20)).find("does-not-exist").click();
+		//withTimeoutOf(Duration.ofSeconds(20)).find("does-not-exist").click();
 		//find("does-not-exist").click();
-		//loginLink.click();
-		//loginLink.waitUntilClickable().click();
+		loginLink.click();
+		loginLink.waitUntilClickable().click();
 	}
 	/*public void SelectMultiState(String... states) {
 		for(String state : states) {
